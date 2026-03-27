12f4c843dddfc3bbda523c736c811bff8aa8345d
I-O-Scarecrow-O-I <mc.0.scarecrow@gmail.com>
2026-03-27 18:36:00 +0800

    ground and player base

---
+++ player/scripts/player.gd
@@ -0,0 +1,15 @@
+class_name  Player extends CharacterBody2D
+
+func _process(delta: float) -> void:
+	
+	pass
+
+func _physics_process(delta: float) -> void:
+	velocity.x=0
+	if Input.is_action_pressed("left"):
+		velocity.x=-100
+	elif Input.is_action_pressed("right"):
+		velocity.x=100
+	velocity.y+=980*delta
+	move_and_slide()
+	pass
